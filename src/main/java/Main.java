7b7c438c84b9ce08e376dfe3425eeac10b17016c
Esperanza <esperanza.aviles@clarity.ai>
2022-09-03 21:00:10 +0200

    style: delete line break in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
         OnlineShop onlineShop = new OnlineShop();
         onlineShop.showProducts();
