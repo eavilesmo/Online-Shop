df0bf1b2cae6083fc37f32832e11e585d0bf45cd
Esperanza <esperanza.aviles@clarity.ai>
2022-09-03 20:57:07 +0200

    feat: create showProducts method in OnlineShop

--- src/main/java/OnlineShop.java
+++ src/main/java/OnlineShop.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class OnlineShop {
+    ProductRepository productRepository = new ProductRepository();
+
+    public void showProducts() {
+        productRepository.addProducts();
+        for (Product product : productRepository.listOfProducts) {
+            System.out.println(product.getImage());
+            System.out.println(product.getShortDescription());
+            System.out.println(product.getHighlightedAttribute());
+            System.out.println(product.getPrice());
+            System.out.println(product.getReference());
+            System.out.println();
+        }
+    }
 }
