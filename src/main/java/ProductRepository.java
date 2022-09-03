9484ffbebfb7c382cade0f3cbecfde99d42e24cc
Esperanza <esperanza.aviles@clarity.ai>
2022-09-03 21:23:51 +0200

    refactor: fixed typos in ProductRepository

--- src/main/java/ProductRepository.java
+++ src/main/java/ProductRepository.java
@@ -18,15 +18,15 @@ public class ProductRepository {
         Product television = new Product(televisionImage, televisionShortDescription, televisionHighlightedAttribute, televisionPrice, televisionReference);
 
         String pianoImage = "\uD83C\uDFB9";
-        String pianoShortDescription = "Untuned musical keyboard, 4 octaves";
-        String pianoHighlightedAttribute = "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
+        String pianoShortDescription = "Off-key musical keyboard, 4 octaves";
+        String pianoHighlightedAttribute = "Tired of your noisy neighbour? Play this off-key musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
         double pianoPrice = 1003.00;
         String pianoReference = "X4A";
         Product piano = new Product(pianoImage, pianoShortDescription, pianoHighlightedAttribute, pianoPrice, pianoReference);
 
         String candleImage = "\uD83D\uDD6F";
         String candleShortDescription = "Relaxing scented candle";
-        String candleHighlightedAttribute = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavander fragance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
+        String candleHighlightedAttribute = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavender fragrance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
         double candlePrice = 4.99;
         String candleReference = "C9Y";
         Product scentedCandle = new Product(candleImage, candleShortDescription, candleHighlightedAttribute, candlePrice, candleReference);
