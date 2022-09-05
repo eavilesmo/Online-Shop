fb0d12c55e679b2f50f39ccd5943d057c9aadfb7
Esperanza <esperanza.aviles@clarity.ai>
2022-09-05 20:29:59 +0200

    feat: make listOfProducts in ProductRepository private

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
-        OnlineShop onlineShop = new OnlineShop();
-        onlineShop.showProducts();
-    }
+
+  public static void main(String[] args) {
+    OnlineShop onlineShop = new OnlineShop();
+    onlineShop.showProducts();
+    onlineShop.showProducts();
+  }
 }
