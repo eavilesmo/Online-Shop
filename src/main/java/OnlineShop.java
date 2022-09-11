cf23fecbb9d2bc0fe4ad2b36a53af0632fc244a3
Esperanza <esperanza.aviles@clarity.ai>
2022-09-11 08:37:35 +0200

    Initial commit in new branch

--- src/main/java/OnlineShop.java
+++ src/main/java/OnlineShop.java
@@ -1,17 +1,17 @@
-import java.util.ArrayList;
-
 public class OnlineShop {
-    ProductRepository productRepository = new ProductRepository();
 
-    public void showProducts() {
-        productRepository.addProducts();
-        for (Product product : productRepository.listOfProducts) {
-            System.out.println(product.getImage());
-            System.out.println(product.getShortDescription());
-            System.out.println(product.getHighlightedAttribute());
-            System.out.printf("\uD83D\uDCB0 Price: %s €\n", product.getPrice());
-            System.out.printf("Reference: %s\n" ,product.getReference());
-            System.out.println();
-        }
+  ProductRepository productRepository = new ProductRepository();
+
+  public void showProducts() {
+    productRepository.addProducts();
+    for (Product product : productRepository.listOfProducts) {
+      System.out.println(product.getImage());
+      System.out.println(product.getShortDescription());
+      System.out.println(product.getHighlightedAttribute());
+      System.out.printf("\uD83D\uDCB0 Price: %s €\n", product.getPrice());
+      System.out.printf("Reference: %s\n", product.getReference());
+      System.out.println();
+            
     }
+  }
 }
