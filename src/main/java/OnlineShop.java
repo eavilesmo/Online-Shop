e6e4e069e2a762291029db514a578b738be18c06
Esperanza <esperanza.aviles@clarity.ai>
2022-09-13 20:33:22 +0200

    feat: created exploreProducts method in OnlineShop

--- src/main/java/OnlineShop.java
+++ src/main/java/OnlineShop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class OnlineShop {
 
   private final ProductRepository productRepository = new ProductRepository();
@@ -6,5 +8,14 @@ public class OnlineShop {
   public void showProducts() {
     String finalProducts = formatter.formatProducts(productRepository.getProducts());
     System.out.println(finalProducts);
+    exploreProducts();
+  }
+
+  private void exploreProducts() {
+    System.out.println("Which product would you like to explore? - Introduce a product's reference.");
+    String userInput = "";
+    Scanner userInputScanner = new Scanner(System.in);
+    userInput = userInputScanner.nextLine();
+    System.out.println(userInput);
   }
 }
