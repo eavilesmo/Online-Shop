994c2bbe2a88c5e249fa75cc8594f36a0f3df662
Esperanza <esperanza.aviles@clarity.ai>
2022-09-14 20:26:46 +0200

    feat: modified exploreProducts methods in OnlineShop

--- src/main/java/OnlineShop.java
+++ src/main/java/OnlineShop.java
@@ -14,8 +14,26 @@ public class OnlineShop {
   private void exploreProducts() {
     System.out.println("Which product would you like to explore? - Introduce a product's reference.");
     Scanner userInputScanner = new Scanner(System.in);
-    String userInput = userInputScanner.nextLine();
-    boolean doesProductExist = productRepository.findProductByReference(userInput);
-    System.out.println(doesProductExist);
+    boolean doesProductExist = false;
+    String userInput = "";
+    while (!doesProductExist) {
+      userInput = userInputScanner.nextLine();
+      doesProductExist = productRepository.findProductByReference(userInput);
+      if (!doesProductExist) {
+        System.out.println("Sorry, the reference does not match any product. Please try again");
+      }
+    }
+    Product singleProductToBeFormatted = productRepository.getSingleProduct(userInput);
+    String singleProduct = formatter.formatSingleProduct(singleProductToBeFormatted);
+    System.out.println(singleProduct);
+
+    System.out.println("What would you like to do next?");
+    System.out.println("1. Add product to cart.");
+    System.out.println("2. Keep browsing the catalog");
+
+    String userInput2 = userInputScanner.nextLine();
+    if (userInput2.equals("2")) {
+      showProducts();
+    }
   }
 }
