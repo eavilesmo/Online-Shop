51512a74aba1a66f40d775783f53eede6c913ab4
Esperanza <esperanza.aviles@clarity.ai>
2022-09-14 19:44:34 +0200

    feat: added longDescription and stock fields to Product

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,35 +1,49 @@
 public class Product {
-    private final String image;
-    private final String shortDescription;
-    private final String highlightedAttribute;
-    private final double price;
-    private final String reference;
 
-    public Product(String image, String shortDescription, String highlightedAttribute, double price, String reference) {
-        this.image = image;
-        this.shortDescription = shortDescription;
-        this.highlightedAttribute = highlightedAttribute;
-        this.price = price;
-        this.reference = reference;
-    }
+  private final String image;
+  private final String shortDescription;
+  private final String highlightedAttribute;
+  private final String longDescription;
+  private final double price;
+  private final String reference;
+  private final int stock;
 
-    public String getImage() {
-        return image;
-    }
+  public Product(String image, String shortDescription, String highlightedAttribute, String longDescription, double price,
+    String reference, int stock) {
+    this.image = image;
+    this.shortDescription = shortDescription;
+    this.highlightedAttribute = highlightedAttribute;
+    this.longDescription = longDescription;
+    this.price = price;
+    this.reference = reference;
+    this.stock = stock;
+  }
 
-    public String getShortDescription() {
-        return shortDescription;
-    }
+  public String getImage() {
+    return image;
+  }
 
-    public String getHighlightedAttribute() {
-        return highlightedAttribute;
-    }
+  public String getShortDescription() {
+    return shortDescription;
+  }
 
-    public double getPrice() {
-        return price;
-    }
+  public String getHighlightedAttribute() {
+    return highlightedAttribute;
+  }
 
-    public String getReference() {
-        return reference;
-    }
+  public String getLongDescription() {
+    return longDescription;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public String getReference() {
+    return reference;
+  }
+
+  public int getStock() {
+    return stock;
+  }
 }
