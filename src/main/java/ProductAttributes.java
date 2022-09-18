44128e1401457954468bfa545019945d5d2e947b
Esperanza <esperanza.aviles@clarity.ai>
2022-09-18 13:18:14 +0200

    refactor: created ProductAttributes class

---
+++ src/main/java/ProductAttributes.java
@@ -0,0 +1,29 @@
+public class ProductAttributes {
+
+  // TV attributes
+  public static final String TV_IMAGE = "\uD83D\uDCFA";
+  public static final String TV_SHORT_DESCRIPTION = "Glamorous panoramic television, 13 inches";
+  public static final String TV_HIGHLIGHTED_ATTRIBUTE = "With this panoramic television, your friday nights will be boring no more.";
+  public static final String TV_LONG_DESCRIPTION = "With this panoramic television, your friday nights will be boring no more. The screen is composed of 14 million pixels. Thanks to its low energy consumption design, your bills will stay low.";
+  public static final double TV_PRICE = 300.99;
+  public static final String TV_REFERENCE = "W2C";
+  public static final int TV_STOCK = 20;
+
+  // Piano attributes
+  public static final String PIANO_IMAGE = "\uD83C\uDFB9";
+  public static final String PIANO_SHORT_DESCRIPTION = "Off-key musical keyboard, 4 octaves";
+  public static final String PIANO_HIGHLIGHTED_ATTRIBUTE = "Tired of your noisy neighbour? Play this off-key musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
+  public static final String PIANO_LONG_DESCRIPTION = "Tired of your noisy neighbour? Play this off-key musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
+  public static final double PIANO_PRICE = 1003.00;
+  public static final String PIANO_REFERENCE = "X4A";
+  public static final int PIANO_STOCK = 15;
+
+  // Candle attributes
+  public static final String CANDLE_IMAGE = "\uD83D\uDD6F";
+  public static final String CANDLE_SHORT_DESCRIPTION = "Relaxing scented candle";
+  public static final String CANDLE_HIGHLIGHTED_ATTRIBUTE = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavender fragrance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
+  public static final String CANDLE_LONG_DESCRIPTION = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavender fragrance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
+  public static final double CANDLE_PRICE = 4.99;
+  public static final String CANDLE_REFERENCE = "C9Y";
+  public static final int CANDLE_STOCK = 145;
+}
--- src/main/java/ProductWarehouse.java
+++ src/main/java/ProductWarehouse.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ProductRepository {
+public class ProductWarehouse {
 
   private final ArrayList<Product> listOfProducts = createProducts();
 
@@ -9,36 +9,29 @@ public class ProductRepository {
   }
 
   private ArrayList<Product> createProducts() {
-    String televisionImage = "\uD83D\uDCFA";
-    String televisionShortDescription = "Glamorous panoramic television, 13 inches";
-    String televisionHighlightedAttribute = "With this panoramic television, your friday nights will be boring no more.";
-    String televisionLongDescription = "With this panoramic television, your friday nights will be boring no more. The screen is composed of 14 million pixels. Thanks to its low energy consumption design, your bills will stay low.";
-    double televisionPrice = 300.99;
-    String televisionReference = "W2C";
-    int televisionStock = 20;
-    Product television = new Product(televisionImage, televisionShortDescription, televisionHighlightedAttribute, televisionLongDescription,
-      televisionPrice,
-      televisionReference, televisionStock);
+    Product television = new Product(ProductAttributes.TV_IMAGE,
+      ProductAttributes.TV_SHORT_DESCRIPTION,
+      ProductAttributes.TV_HIGHLIGHTED_ATTRIBUTE,
+      ProductAttributes.TV_LONG_DESCRIPTION,
+      ProductAttributes.TV_PRICE,
+      ProductAttributes.TV_REFERENCE,
+      ProductAttributes.TV_STOCK);
 
-    String pianoImage = "\uD83C\uDFB9";
-    String pianoShortDescription = "Off-key musical keyboard, 4 octaves";
-    String pianoHighlightedAttribute = "Tired of your noisy neighbour? Play this off-key musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
-    String pianoLongDescription = "Tired of your noisy neighbour? Play this off-key musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
-    double pianoPrice = 1003.00;
-    String pianoReference = "X4A";
-    int pianoStock = 15;
-    Product piano = new Product(pianoImage, pianoShortDescription, pianoHighlightedAttribute, pianoLongDescription, pianoPrice,
-      pianoReference, pianoStock);
+    Product piano = new Product(ProductAttributes.PIANO_IMAGE,
+      ProductAttributes.PIANO_SHORT_DESCRIPTION,
+      ProductAttributes.PIANO_HIGHLIGHTED_ATTRIBUTE,
+      ProductAttributes.PIANO_LONG_DESCRIPTION,
+      ProductAttributes.PIANO_PRICE,
+      ProductAttributes.PIANO_REFERENCE,
+      ProductAttributes.PIANO_STOCK);
 
-    String candleImage = "\uD83D\uDD6F";
-    String candleShortDescription = "Relaxing scented candle";
-    String candleHighlightedAttribute = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavender fragrance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
-    String candleLongDescription = "If you feel stressed out at the end of the day, try giving yourself a break with our relaxing scented candle! With a subtle lavender fragrance, get rid of all the stress. Some customers affirm that this candle helps them to solve bugs in their code.";
-    double candlePrice = 4.99;
-    String candleReference = "C9Y";
-    int candleStock = 145;
-    Product scentedCandle = new Product(candleImage, candleShortDescription, candleHighlightedAttribute, candleLongDescription, candlePrice,
-      candleReference, candleStock);
+    Product scentedCandle = new Product(ProductAttributes.CANDLE_IMAGE,
+      ProductAttributes.CANDLE_SHORT_DESCRIPTION,
+      ProductAttributes.CANDLE_HIGHLIGHTED_ATTRIBUTE,
+      ProductAttributes.CANDLE_LONG_DESCRIPTION,
+      ProductAttributes.CANDLE_PRICE,
+      ProductAttributes.CANDLE_REFERENCE,
+      ProductAttributes.CANDLE_STOCK);
 
     ArrayList<Product> productsToBeAdded = new ArrayList<>();
     productsToBeAdded.add(television);
