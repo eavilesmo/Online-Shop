abd2beea2c2618bd1f4c1a1c998a6b3cb2d02f1a
Esperanza <esperanza.aviles@clarity.ai>
2022-09-18 16:51:56 +0200

    refactor: extracted small methods in Formatter class

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -3,56 +3,74 @@ import java.util.ArrayList;
 public class Formatter {
 
   public String formatProducts(ArrayList<Product> listOfProducts) {
-    StringBuilder finalProducts = new StringBuilder();
+    StringBuilder formattedStrings = new StringBuilder();
     for (Product product : listOfProducts) {
-      finalProducts.append(product.getImage());
-      finalProducts.append(StringRepository.LINE_BREAK);
-
-      finalProducts.append(product.getShortDescription());
-      finalProducts.append(StringRepository.LINE_BREAK);
-
-      finalProducts.append(product.getHighlightedAttribute());
-      finalProducts.append(StringRepository.LINE_BREAK);
-
-      String formatForPrice = StringRepository.FORMAT_FOR_PRICE;
-      String price = String.format(formatForPrice, product.getPrice());
-      finalProducts.append(price);
-
-      String formatForReference = StringRepository.FORMAT_FOR_REFERENCE;
-      String reference = String.format(formatForReference, product.getReference());
-      finalProducts.append(reference);
-      finalProducts.append(StringRepository.LINE_BREAK);
+      addProductImage(formattedStrings, product);
+      addProductShortDescription(formattedStrings, product);
+      addProductHighlightedAttribute(formattedStrings, product);
+      addProductPrice(formattedStrings, product);
+      addProductReference(formattedStrings, product);
     }
-    return finalProducts.toString();
+    return formattedStrings.toString();
   }
 
   public String formatProductDetails(Product product) {
-    StringBuilder singleProduct = new StringBuilder();
-    singleProduct.append(product.getImage());
-    singleProduct.append(StringRepository.LINE_BREAK);
+    StringBuilder formattedString = new StringBuilder();
+    addProductImage(formattedString, product);
+    addProductPrice(formattedString, product);
+    addProductReference(formattedString, product);
+    addProductStock(formattedString, product);
+    addProductSummary(formattedString, product);
+    addProductGeneralDescription(formattedString, product);
 
+    return formattedString.toString();
+  }
+
+  private void addProductImage(StringBuilder stringBuilder, Product product) {
+    stringBuilder.append(product.getImage());
+    stringBuilder.append(StringRepository.LINE_BREAK);
+  }
+
+  private void addProductShortDescription(StringBuilder stringBuilder, Product product) {
+    stringBuilder.append(product.getShortDescription());
+    stringBuilder.append(StringRepository.LINE_BREAK);
+  }
+
+  private void addProductHighlightedAttribute(StringBuilder stringBuilder, Product product) {
+    stringBuilder.append(product.getHighlightedAttribute());
+    stringBuilder.append(StringRepository.LINE_BREAK);
+  }
+
+  private void addProductPrice(StringBuilder stringBuilder, Product product) {
     String formatForPrice = StringRepository.FORMAT_FOR_PRICE;
     String price = String.format(formatForPrice, product.getPrice());
-    singleProduct.append(price);
+    stringBuilder.append(price);
+  }
 
+  private void addProductReference(StringBuilder stringBuilder, Product product) {
     String formatForReference = StringRepository.FORMAT_FOR_REFERENCE;
     String reference = String.format(formatForReference, product.getReference());
-    singleProduct.append(reference);
+    stringBuilder.append(reference);
+    stringBuilder.append(StringRepository.LINE_BREAK);
+  }
 
+  private void addProductStock(StringBuilder stringBuilder, Product product) {
     String formatForStock = StringRepository.FORMAT_FOR_STOCK;
     String stock = String.format(formatForStock, product.getStock());
-    singleProduct.append(stock).append(StringRepository.LINE_BREAK);
+    stringBuilder.append(stock).append(StringRepository.LINE_BREAK);
+  }
 
-    singleProduct.append(StringRepository.SUMMARY_TITLE);
-    singleProduct.append(product.getShortDescription());
-    singleProduct.append(StringRepository.LINE_BREAK);
-    singleProduct.append(product.getHighlightedAttribute());
-    singleProduct.append(StringRepository.DOUBLE_LINE_BREAK);
+  private void addProductSummary(StringBuilder stringBuilder, Product product) {
+    stringBuilder.append(StringRepository.SUMMARY_TITLE);
+    stringBuilder.append(product.getShortDescription());
+    stringBuilder.append(StringRepository.LINE_BREAK);
+    stringBuilder.append(product.getHighlightedAttribute());
+    stringBuilder.append(StringRepository.DOUBLE_LINE_BREAK);
+  }
 
-    singleProduct.append(StringRepository.DESCRIPTION_TITLE);
-    singleProduct.append(product.getLongDescription());
-    singleProduct.append(StringRepository.DOUBLE_LINE_BREAK);
-
-    return singleProduct.toString();
+  private void addProductGeneralDescription(StringBuilder stringBuilder, Product product) {
+    stringBuilder.append(StringRepository.DESCRIPTION_TITLE);
+    stringBuilder.append(product.getLongDescription());
+    stringBuilder.append(StringRepository.DOUBLE_LINE_BREAK);
   }
 }
