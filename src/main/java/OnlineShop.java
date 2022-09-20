07d3f411155ff3ca07f672e108e90dfca4871956
Esperanza <esperanza.aviles@clarity.ai>
2022-09-20 21:22:10 +0200

    refactor: moved methods to ActionExplorer class

--- src/main/java/OnlineShop.java
+++ src/main/java/OnlineShop.java
@@ -1,51 +1,12 @@
-import java.util.Scanner;
-
 public class OnlineShop {
 
-  private final ProductWarehouse productWarehouse = new ProductWarehouse();
-  private final Formatter formatter = new Formatter();
+  private static final ProductWarehouse productWarehouse = new ProductWarehouse();
+  private static final Formatter formatter = new Formatter();
+  private static final ActionExplorer actionExplorer = new ActionExplorer(productWarehouse, formatter);
 
-
-  public void showCatalog() {
+  public static void showCatalog() {
     String finalProducts = formatter.formatProducts(productWarehouse.getProducts());
     System.out.println(finalProducts);
-    browseProductsByReference();
-    executeNextActions();
-  }
-
-  private void browseProductsByReference() {
-    System.out.println(StringRepository.EXPLORE_PRODUCTS);
-    Scanner scanner = new Scanner(System.in);
-    boolean doesProductExist = false;
-    String reference = "";
-    while (!doesProductExist) {
-      reference = scanner.nextLine();
-      doesProductExist = productWarehouse.doesProductExist(reference);
-      if (!doesProductExist) {
-        System.out.println(StringRepository.INCORRECT_REFERENCE);
-      }
-    }
-    Product singleProductToBeFormatted = productWarehouse.findProductByReference(reference);
-    String singleProduct = formatter.formatProductDetails(singleProductToBeFormatted);
-    System.out.println(singleProduct);
-  }
-
-  private void executeNextActions() {
-    printNextActions();
-    Scanner scanner = new Scanner(System.in);
-    String shopperAction = scanner.nextLine();
-    if (shopperWantsToKeepBrowsingTheCatalog(shopperAction)) {
-      showCatalog();
-    }
-  }
-
-  private void printNextActions() {
-    System.out.println(StringRepository.NEXT_ACTION_QUESTION);
-    System.out.println(StringRepository.ADD_PRODUCT_TO_CART);
-    System.out.println(StringRepository.KEEP_BROWSING_PRODUCTS);
-  }
-
-  private boolean shopperWantsToKeepBrowsingTheCatalog(String shopperAction) {
-    return shopperAction.equals(StringRepository.SECOND_OPTION);
+    actionExplorer.exploreNextActions();
   }
 }
