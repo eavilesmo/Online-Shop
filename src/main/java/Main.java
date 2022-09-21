bc8b7ce4011b7d2647f92ad3cc721258b3b12d84
Esperanza <esperanza.aviles@clarity.ai>
2022-09-21 19:29:05 +0200

    refactor: made static showCatalog method from OnlineShop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-    OnlineShop onlineShop = new OnlineShop();
-    onlineShop.showProducts();
+    OnlineShop.showCatalog();
   }
 }
