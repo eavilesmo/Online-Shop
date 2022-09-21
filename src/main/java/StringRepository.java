d8f9fb9752fe46c757d0813df437ead611249aa2
Esperanza <esperanza.aviles@clarity.ai>
2022-09-21 18:52:12 +0200

    feat: added StringRepository class

---
+++ src/main/java/StringRepository.java
@@ -0,0 +1,19 @@
+public class StringRepository {
+
+  public static final String EXPLORE_PRODUCTS = "Which product would you like to explore? - Introduce a product's reference.";
+  public static final String INCORRECT_REFERENCE = "Sorry, the reference does not match any product. Please try again";
+  public static final String NEXT_ACTION_QUESTION = "What would you like to do next?";
+  public static final String ADD_PRODUCT_TO_CART = "1. Add product to cart.";
+  public static final String KEEP_BROWSING_PRODUCTS = "2. Keep browsing the catalog";
+  public static final String SECOND_OPTION = "2";
+
+  public static final String FORMAT_FOR_PRICE = "\uD83D\uDCB0 Price: %s €\n";
+  public static final String FORMAT_FOR_REFERENCE = "Reference: %s \n";
+  public static final String FORMAT_FOR_STOCK = "%s left.\n";
+  public static final String SUMMARY_TITLE = "SUMMARY:\n";
+  public static final String DESCRIPTION_TITLE = "DESCRIPTION:\n";
+  public static final String LINE_BREAK = "\n";
+  public static final String DOUBLE_LINE_BREAK = "\n\n";
+
+  public static final String PRODUCT_DOES_NOT_EXIST = "The product does not exist in the database";
+}
